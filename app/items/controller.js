c1d80cf861731f3fae722976cffd69a2dec557af
Markham Shofner <markham.shofner@gmail.com>
2017-03-05 14:23:16 -0500

    finish app and architecture sections

---
+++ app/items/controller.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+  actions: {
+    doSearch (q) {
+      // NOTE: don't need to pass route name b/c same route
+      this.transitionToRoute('items', {
+        queryParams: { q }
+      });
+    }
+  }
+});
---
+++ app/items/route.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  // changes to these query parameter will cause this route to
+  // update the model by calling the "model()" hook again
+  queryParams: {
+    q: { refreshModel: true }
+  },
+  
+  // from ember-arcgis-portal-services
+  itemsService: Ember.inject.service('items-service'),
+
+  // the model hook is used to fetch any data based on route parameters
+  model (params) {
+    const itemsService = this.get('itemsService');
+    const q = params.q || '*';
+    return itemsService.search({ q });
+  }
+});
